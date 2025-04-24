03e2a1c4207b6e91efef937b55fc9fc2a695b76d
fiahz <soherrerazu@est.utn.ac.cr>
2025-04-24 10:36:21 -0600

    PButtons
    implementación de de PButtons para agregar y eliminar imagen, todavia incompleto

---
+++ src/Pages/PButtons.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Buttons from "../Components/Buttons";
+import Task from "../Components/Task"
+import TaskList from "../Components/TaskList"
+
+const PButtons = () => {
+
+    const [showList, setShowList] = useState (false);
+
+    const addList = () => setShowList(true);
+    const deleteList = () => setShowList(false);
+
+    return(
+        <div className="text-center p-4"> 
+         <Buttons onAgregar={addList} onEliminar={deleteList} />
+         <Task />
+         <TaskList />
+        </div>
+    )
+
+
+    
+}
+
+export default PButtons;
