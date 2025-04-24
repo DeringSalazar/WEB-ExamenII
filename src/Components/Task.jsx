3abdc864ef3d918b715256279fe40aadf7c80b27
DeringSalazar <desalazarsa@est.utn.ac.cr>
2025-04-24 09:28:21 -0600

    Se crea la carpeta Components y Pages
    En este paso en la carpeta Components se agregaron los archivos del TaskList y Task

---
+++ src/Components/Task.jsx
@@ -0,0 +1,11 @@
+const Task = ({ text, state}) => {
+    return (
+        <>
+            <ol className="list-group list-group-numbered">
+                <li className="list-group-item" >Cras justo odio</li>
+            </ol>
+        </>
+    );
+};
+
+export default Task;
