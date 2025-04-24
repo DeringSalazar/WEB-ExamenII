f9833da784b829f168f8af7c130e0c0b33ddcdc0
DeringSalazar <desalazarsa@est.utn.ac.cr>
2025-04-24 10:13:48 -0600

    Se crean los import de App
    se crea el import de PaTaskList

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import { BrouserRouter, Router, Route } from 'react-router-dom'
 import './App.css'
 import PaTaskList from './Pages/PaTaskList'
+import Task from './Components/Task'
 
 function App() {
+
+
 
   return (
     <BrouserRouter>
       <Router>
-        <Route path="/Pages/PaTaskList" component={PaTaskList}></Route>
+        <Route path="/Pages/PaTaskList" element={<PaTaskList/>}></Route>
+        <Route path="/Components/Task" element={<Task/>}></Route>
       </Router>
     </BrouserRouter>
   )
